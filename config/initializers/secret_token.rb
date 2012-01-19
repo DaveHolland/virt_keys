e729a5b6ad6e1c07b0068f9a5f8da348cd6ce2d8
David Holland <DaveHolland@stanfordalumni.org>
2012-01-19 16:28:29 -0500

    Initial virtkeys commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VirtKeys::Application.config.secret_token = '0ed930123b504bb57f165e969b52779963f8ab64ee0d23a8e6e36805622e4c8022313c0e95e3070ea8d0646e5d026f09f30ba5fc4c28af89d6ba01966147d0da'
